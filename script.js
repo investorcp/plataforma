83359bff807c6db8f5dabc8bd78ae5f4e977bc1a
Paulo Moises <paulomoises_silva@hotmail.com>
2019-08-06 14:36:05 -0300

    a

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
-   $("body").delegate("#newPropose", "click", function() {
-       navegar("novaProposta")
-   })
+$("body").delegate("#newPropose", "click", function() {
+    navegar("novaProposta")
+})
 
-   $("body").delegate("#newClient", "click", function() {
-       navegar("novoCliente")
-   })
+$("body").delegate("#newClient", "click", function() {
+    $.get("src/components/clientes/novo-clientes.html", function(data) {
+        window.history.pushState(data, "Plataforma Investor", "src/components/clientes/novo-clientes.html")
+        $("#body").html(data)
+    })
+})
 
-   $("#cliente").on("click", function() {
-       $.get("src/components/Clientes/clientes.html", function(data) {
-           $("#body").html(data)
-       })
-   })
+$("#cliente").on("click", function() {
+    $.get("src/components/clientes/clientes.html", function(data) {
+        $("#body").html(data)
+    })
+})
 
-   $(function() {
-       $('.input-data').datepicker({
-           format: 'dd/mm/yyyy'
-       });
-   });
+$("#proposta").on("click", function() {
+    $.get("src/components/proposta/proposta.html", function(data) {
+        $("#body").html(data)
+    })
+})
 
-   $(function() {
-       let data = new Date()
-       let ano = data.getFullYear() - 1
-       let lAno = ano - 2
-       let frase = lAno + " à " + ano
-       $('.ultimos3anos').val(frase)
-   });
+$(function() {
+    $('.input-data').datepicker({
+        format: 'dd/mm/yyyy'
+    });
+});
+
+$(function() {
+    let data = new Date()
+    let ano = data.getFullYear() - 1
+    let lAno = ano - 2
+    let frase = lAno + " à " + ano
+    $('.ultimos3anos').val(frase)
+});
 
 
-   (function(global, factory) {
-       if (typeof define === 'function' && define.amd) {
-           define(['../numeral'], factory);
-       } else if (typeof module === 'object' && module.exports) {
-           factory(require('../numeral'));
-       } else {
-           factory(global.numeral);
-       }
-   }(this, function(numeral) {
-       numeral.register('locale', 'pt-br', {
-           delimiters: {
-               thousands: '.',
-               decimal: ','
-           },
-           abbreviations: {
-               thousand: 'mil',
-               million: 'milhões',
-               billion: 'b',
-               trillion: 't'
-           },
-           ordinal: function(number) {
-               return 'º';
-           },
-           currency: {
-               symbol: 'R$'
-           }
-       });
-   }));
+(function(global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['../numeral'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('../numeral'));
+    } else {
+        factory(global.numeral);
+    }
+}(this, function(numeral) {
+    numeral.register('locale', 'pt-br', {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'mil',
+            million: 'milhões',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function(number) {
+            return 'º';
+        },
+        currency: {
+            symbol: 'R$'
+        }
+    });
+}));
