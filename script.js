9d13b4881dc617d3c9886d7afdd218509812b2b1
ediedias <53578736+ediedias@users.noreply.github.com>
2019-08-23 17:00:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -125,3 +125,21 @@ $(function() {
         }
     });
 }));
+
+
+$(function() {
+	var check = $.cookie('logado');
+	if (check!="true")
+	{
+		window.location.replace("http://plataforma.grupoinvestor.com/login.html");		
+	}	
+});
+
+
+function logout() {
+	$.removeCookie('logado');
+	$.removeCookie('nome');
+	$.removeCookie('setor');
+	$.removeCookie('cargo');
+	location.reload();
+}
