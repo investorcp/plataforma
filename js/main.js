4155e008c83aae1552b67a3f597cd8120a21d3d7
Paulo Moises <paulomoises_silva@hotmail.com>
2019-08-27 15:10:04 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+$(function() {
+    var url = window.location.href
+    var htt = url.split(":")
+    var ht0 = (htt[1]).split(".")
+    if (htt[0] == "http" && ht0[0] == "//plataforma") {
+        var u0 = url.replace("http", "https")
+        window.location.href = u0
+    }
+})
 
-(function ($) {
+(function($) {
     "use strict";
 
 
@@ -7,53 +16,52 @@
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function() {
         var check = true;
-		var usuario = $(input[0]).val();
-		var senha = $(input[1]).val();
-		var endlogin = 'https://script.google.com/macros/s/AKfycbwaw20aQLJAlPmcLV2IH9fo8HQvXBAzjRJ_3ORodHU5zLFiIYs/exec?usuario=' + usuario + '&senha=' + senha;
+        var usuario = $(input[0]).val();
+        var senha = $(input[1]).val();
+        var endlogin = 'https://script.google.com/macros/s/AKfycbwaw20aQLJAlPmcLV2IH9fo8HQvXBAzjRJ_3ORodHU5zLFiIYs/exec?usuario=' + usuario + '&senha=' + senha;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
-		if(check==true) {
-			$.getJSON(endlogin, function(data) {
-				if(data['auth']==true) {
-					//Cookies.set('logado', 'true');
-					$.cookie('logado', "true");
-					$.cookie('nome',data['nome']);
-					$.cookie('setor',data['setor']);
-					$.cookie('cargo',data['cargo']);
-					//Redireciona para a pagina principal
-					window.location.replace("https://plataforma.grupoinvestor.com/");
-					
-				} else {
-					console.log("login invalido");
-					$(".error").text("Login Inválido");
-				}
-			});			
-		}
+        if (check == true) {
+            $.getJSON(endlogin, function(data) {
+                if (data['auth'] == true) {
+                    //Cookies.set('logado', 'true');
+                    $.cookie('logado', "true");
+                    $.cookie('nome', data['nome']);
+                    $.cookie('setor', data['setor']);
+                    $.cookie('cargo', data['cargo']);
+                    //Redireciona para a pagina principal
+                    window.location.replace("https://plataforma.grupoinvestor.com/");
+
+                } else {
+                    console.log("login invalido");
+                    $(".error").text("Login Inválido");
+                }
+            });
+        }
         return false;
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    $('.validate-form .input100').each(function() {
+        $(this).focus(function() {
+            hideValidate(this);
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
-        }
-        else {
-            if($(input).val().trim() == ''){
+        } else {
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -70,27 +78,26 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-	
-	
+
+
+
     /*==================================================================
     [ Show pass ]*/
     var showPass = 0;
-    $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-            $(this).next('input').attr('type','text');
+    $('.btn-show-pass').on('click', function() {
+        if (showPass == 0) {
+            $(this).next('input').attr('type', 'text');
             $(this).find('i').removeClass('fa-eye');
             $(this).find('i').addClass('fa-eye-slash');
             showPass = 1;
-        }
-        else {
-            $(this).next('input').attr('type','password');
+        } else {
+            $(this).next('input').attr('type', 'password');
             $(this).find('i').removeClass('fa-eye-slash');
             $(this).find('i').addClass('fa-eye');
             showPass = 0;
         }
-        
+
     });
-    
+
 
 })(jQuery);
